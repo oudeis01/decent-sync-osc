a2ccbc1e53a204cf39162de4afcbcc4c42b08079
choiharam <altitudinem@gmail.com>
2025-03-24 16:27:27 +0100

    test: info as str

--- sender.cpp
+++ sender.cpp
@@ -28,6 +28,7 @@ void Sender::sendDone(const std::string& ip, int port, int index) {
 void Sender::sendInfo(const std::string& ip, int port, const std::queue<Command>& queue) {
     lo_address addr = lo_address_new(ip.c_str(), std::to_string(RESPONSE_PORT).c_str());
     lo_message msg = lo_message_new();
+    std::string qstr = "";
 
     if (queue.empty()) {
         lo_message_add_string(msg, "no commands in the queue");
@@ -38,31 +39,38 @@ void Sender::sendInfo(const std::string& ip, int port, const std::queue<Command>
             const Command& cmd = q_copy.front();
             switch (cmd.type) {
                 case Command::ROTATE:
-                    lo_message_add_string(msg, "rotate");
-                    lo_message_add_int32(msg, cmd.steps);
-                    lo_message_add_float(msg, cmd.delayUs);  // Send as float
-                    lo_message_add_int32(msg, cmd.direction);
+                    // lo_message_add_string(msg, "rotate");
+                    // lo_message_add_int32(msg, cmd.steps);
+                    // lo_message_add_float(msg, cmd.delayUs);  // Send as float
+                    // lo_message_add_int32(msg, cmd.direction);
+                    qstr += "rotate " + std::to_string(cmd.steps) + " steps at " + std::to_string(cmd.delayUs) + " us delay, direction " + std::to_string(cmd.direction) + "\n";
                     break;
                 case Command::ENABLE:
-                    lo_message_add_int32(msg, cmd.index);
-                    lo_message_add_string(msg, "enable");
+                    // lo_message_add_int32(msg, cmd.index);
+                    // lo_message_add_string(msg, "enable");
+                    qstr += "enable motor #" + std::to_string(cmd.index) + "\n";
                     break;
                 case Command::DISABLE:
-                    lo_message_add_int32(msg, cmd.index);
-                    lo_message_add_string(msg, "disable");
+                    // lo_message_add_int32(msg, cmd.index);
+                    // lo_message_add_string(msg, "disable");
+                    qstr += "disable motor #" + std::to_string(cmd.index) + "\n";
                     break;
                 case Command::INFO:
-                    lo_message_add_int32(msg, cmd.index);
-                    lo_message_add_string(msg, "info");
+                    // lo_message_add_int32(msg, cmd.index);
+                    // lo_message_add_string(msg, "info");
+                    qstr += "get info for motor #" + std::to_string(cmd.index) + "\n";
                     break;
                 case Command::EXIT:
-                    lo_message_add_int32(msg, cmd.index);
-                    lo_message_add_string(msg, "exit");
+                    // lo_message_add_int32(msg, cmd.index);
+                    // lo_message_add_string(msg, "exit");
+                    qstr += "exit\n";
                     break;
             }
             q_copy.pop();
         }
+        lo_message_add_string(msg, qstr.c_str());
         lo_send_message(addr, "/info", msg);
+
     }
     lo_message_free(msg);
     lo_address_free(addr);
